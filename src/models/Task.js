bc10c7dce24f137353e4520848f4c51942e8fd1e
ikipm <ikipm0@gmail.com>
2022-06-29 23:28:09 +0200

    Add: DB task model

---
+++ src/models/Task.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+// Create each element and add time (created and updated) and erases the version Key (__V).
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default model("Task", taskSchema);
