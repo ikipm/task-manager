17ade7aad634582baaf3a8a711b1cdbedb4772ce
ikipm <ikipm0@gmail.com>
2022-06-29 23:27:34 +0200

    Update: Catch errors on db connection

--- src/database.js
+++ src/database.js
@@ -1,5 +1,12 @@
 import { connect } from "mongoose";
 
-// Connects with localhost mongodb, to the crud-mongo db. When the connection is finished continues running the code.
-const db = await connect("mongodb://localhost/crud-mongo");
-console.log("DB connection established to", db.connection.name);
+// Create a lambda function to use async/await
+(async () => {
+  try {
+    // Connects with localhost mongodb, to the crud-mongo db. When the connection is finished continues running the code.
+    const db = await connect("mongodb://localhost/crud-mongo");
+    console.log("DB connection established to", db.connection.name);
+  } catch (error) {
+    console.error(error);
+  }
+})();
