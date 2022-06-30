bc99bbe08fe8c26814b8c0c9d7abafa19c8ff000
ikipm <ikipm0@gmail.com>
2022-06-30 23:05:17 +0200

    Add: Post router for /tasks/add

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import TaskModel from "../models/Task";
 
 const router = Router();
 
+// Get routers
 router.get("/", (req, res) => {
   res.render("index"); // rendering index.hbs file when user visits /
 });
@@ -12,6 +14,12 @@ router.get("/about", (req, res) => {
 
 router.get("/edit", (req, res) => {
   res.render("edit"); // rendering edit.hbs file when user visits /edit
+});
+
+// Post routers
+router.post("/tasks/add", async (req, res) => {
+  const taskSaved = await TaskModel(req.body).save(); // Save the task into the db.
+  res.send("Added task"); // return Added task when getting a post to /tasks/add
 });
 
 export default router;
