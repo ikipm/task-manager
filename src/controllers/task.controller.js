68308e4fd6c1360a9c6b8b4e0419af865d9c5441
ikipm <ikipm0@gmail.com>
2022-07-06 13:42:27 +0200

    Update: Router refactor

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,52 @@
+import TaskModel from "../models/Task";
+
+// Add the task
+const AddTask = async (req, res) => {
+  try {
+    await TaskModel(req.body).save(); // Save the task into the db.
+    res.redirect("/"); // Redirect to home when finishing.
+  } catch (error) {
+    console.error(error); // if there is an error, show it
+  }
+};
+
+// Delete the task
+const DeleteTask = async (req, res) => {
+  await TaskModel.findByIdAndDelete(req.params.id); // find a task by the id and delete it.
+  res.redirect("/"); // redirect to index.hbs file
+};
+
+// Edit tasks
+const EditTask = async (req, res) => {
+  await TaskModel.findByIdAndUpdate(req.params.id, req.body); // Update the task by the id
+  res.redirect("/"); // redirect to index.hbs file
+};
+
+const RenderTaskEdit = async (req, res) => {
+  try {
+    const task = await TaskModel.findById(req.params.id).lean(); // getting tasks from the db by the id and converting to normal js object.
+    if (task) {
+      res.render("edit", { task }); // rendering edit.hbs file when user visits /edit
+    } else {
+      res.redirect("/"); // redirect to index.hbs file
+    }
+  } catch (error) {
+    console.error(error); // if there is an error, show it
+  }
+};
+
+// Load tasks
+const LoadTasks = async (req, res) => {
+  const tasks = await TaskModel.find().lean(); // getting tasks from the db and converting to normal js object.
+  res.render("index", { tasks }); // rendering index.hbs file when user visits /
+};
+
+// ToggleDone from tasks
+const ToggleDone = async (req, res) => {
+  const task = await TaskModel.findById(req.params.id);
+  task.done = !task.done;
+  await task.save();
+  res.redirect("/"); // redirect to index.hbs file
+};
+
+export { AddTask, DeleteTask, EditTask, RenderTaskEdit, LoadTasks, ToggleDone };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,56 +1,27 @@
 import { Router } from "express";
-import TaskModel from "../models/Task";
+import {
+  AddTask,
+  DeleteTask,
+  EditTask,
+  LoadTasks,
+  RenderTaskEdit,
+  ToggleDone,
+} from "../controllers/task.controller";
 
 const router = Router();
 
 // Get routers
-router.get("/", async (req, res) => {
-  const tasks = await TaskModel.find().lean(); // getting tasks from the db and converting to normal js object.
-  res.render("index", { tasks }); // rendering index.hbs file when user visits /
-});
+router.get("/", LoadTasks);
 
-router.get("/about", (req, res) => {
-  res.render("about"); // rendering about.hbs file when user visits /about
-});
+router.get("/task/edit/:id", RenderTaskEdit);
 
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const task = await TaskModel.findById(req.params.id).lean(); // getting tasks from the db by the id and converting to normal js object.
-    if (task) {
-      res.render("edit", { task }); // rendering edit.hbs file when user visits /edit
-    } else {
-      res.redirect("/"); // redirect to index.hbs file
-    }
-  } catch (error) {
-    console.error(error); // if there is an error, show it
-  }
-});
+router.get("/task/delete/:id", DeleteTask);
 
-router.get("/delete/:id", async (req, res) => {
-  await TaskModel.findByIdAndDelete(req.params.id); // find a task by the id and delete it.
-  res.redirect("/"); // redirect to index.hbs file
-});
-
-router.get("/done/:id", async (req, res) => {
-  const task = await TaskModel.findById(req.params.id);
-  task.done = !task.done;
-  await task.save();
-  res.redirect("/"); // redirect to index.hbs file
-});
+router.get("/task/done/:id", ToggleDone);
 
 // Post routers
-router.post("/tasks/add", async (req, res) => {
-  try {
-    await TaskModel(req.body).save(); // Save the task into the db.
-    res.redirect("/"); // Redirect to home when finishing.
-  } catch (error) {
-    console.error(error); // if there is an error, show it
-  }
-});
+router.post("/task/add", AddTask);
 
-router.post("/edit/:id", async (req, res) => {
-  await TaskModel.findByIdAndUpdate(req.params.id, req.body); // Update the task by the id
-  res.redirect("/"); // redirect to index.hbs file
-});
+router.post("/task/edit/:id", EditTask);
 
 export default router;
