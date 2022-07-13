08b3e2e6679d39d4e921b5a488238029a03502c8
Iker Pérez <ikipm0@gmail.com>
2022-07-13 11:06:03 +0200

    Refactor: config file

--- src/config.js
+++ src/config.js
@@ -2,9 +2,12 @@ import { config } from "dotenv";
 
 config();
 
+const env = process.env;
+
 // Get mongo url. If it can't find the url then it's mongodb://localhost/crud-mongo
-export const MONGODB_URL =
-  process.env.MONGODB_URL || "mongodb://localhost/crud-mongo";
+const MONGODB_URL = env.MONGODB_URL || "mongodb://localhost/crud-mongo";
 
 // Get port from .env. If it can't find the port then it's 3000
-export const PORT = process.env.PORT || 3000;
+const PORT = env.PORT || 3000;
+
+export { MONGODB_URL, PORT };
